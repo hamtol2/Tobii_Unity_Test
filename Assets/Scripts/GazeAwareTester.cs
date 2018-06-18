52b801ad9696a9c48fd3e5eb23d09b6c9c36dcfd
Ronnie Jang <yenseb@hanmail.net>
2018-06-18 14:30:43 +0900

    Initial commit

---
+++ Assets/Scripts/GazeAwareTester.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tobii.Gaming;
+using UnityEngine;
+
+namespace RonnieJ
+{
+    public class GazeAwareTester : MonoBehaviour
+    {
+        public Color normalColor = Color.white;
+        public Color focusedColor = Color.red;
+
+        public GazeAware gazeAware;
+        public Renderer targetRenderer;
+
+        private bool hasFocus = false;
+
+        void Awake()
+        {
+            if (gazeAware == null)
+                gazeAware = GetComponent<GazeAware>();
+
+            if (targetRenderer == null)
+                targetRenderer = GetComponent<Renderer>();
+        }
+
+        void Update()
+        {
+            UpdateState();
+        }
+
+        void UpdateState()
+        {
+            if (gazeAware.HasGazeFocus != hasFocus)
+            {
+                hasFocus = gazeAware.HasGazeFocus;
+                ChangeColor(hasFocus);
+            }
+        }
+
+        void ChangeColor(bool state)
+        {
+            if (state) targetRenderer.material.color = focusedColor;
+            else targetRenderer.material.color = normalColor;
+        }
+    }
+}
---
+++ Assets/Scripts/GazeMove.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tobii.Gaming;
+using UnityEngine;
+
+namespace RonnieJ
+{
+    public class GazeMove : MonoBehaviour
+    {
+        public enum State
+        {
+            Idle, Move
+        }
+
+        public State state = State.Idle;
+
+        public Transform marker;
+        public float moveSpeed = 3f;
+        public float turnSpeed = 540f;
+
+        public float frame = 30f;
+
+        private float fps = 0f;
+        private bool hasStarted = false;
+        private float startedTime = 0f;
+        private Timer timer = new Timer();
+        private float distance = 10f;
+
+        void Awake()
+        {
+            fps = 1f / frame;
+        }
+
+        void InitTobii(GazePoint gazePoint)
+        {
+            hasStarted = true;
+            startedTime = gazePoint.Timestamp;
+
+            marker.gameObject.SetActive(true);
+        }
+
+        void Update()
+        {
+            timer.Update(Time.deltaTime);
+            if (timer.HasPastSince(fps))
+            {
+                UpdateMarkerPosition();
+            }
+
+            ProcessInput();
+            FSM();
+        }
+
+        void SetState(State newState)
+        {
+            state = newState;
+        }
+
+        void FSM()
+        {
+            switch (state)
+            {
+                case State.Idle: Idle(); break;
+                case State.Move: Move(); break;
+                default: break;
+            }
+        }
+
+        void Idle()
+        {
+
+        }
+
+        void Move()
+        {
+            Vector3 dir = marker.position - transform.position;
+            dir.y = 0f;
+
+            RotateToward(dir);
+
+            Vector3 targetPos = marker.position;
+            targetPos.y = transform.position.y;
+            Vector3 framePos = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            transform.position = framePos;
+        }
+
+        void RotateToward(Vector3 dir)
+        {
+            if (dir == Vector3.zero)
+                return;
+
+            Quaternion targetRot = Quaternion.LookRotation(dir);
+            Quaternion frameRot = Quaternion.RotateTowards(transform.rotation, targetRot, turnSpeed * Time.deltaTime);
+            transform.rotation = frameRot;
+        }
+
+        bool HasArrived
+        {
+            get
+            {
+                Vector3 startPos = transform.position;
+                startPos.y = 0f;
+                Vector3 endPos = marker.position;
+                endPos.y = 0f;
+
+                return Vector3.Distance(startPos, endPos) <= 0.5f;
+            }
+        }
+
+        void ProcessInput()
+        {
+            // Toggle State.
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (state == State.Idle) SetState(State.Move);
+                else SetState(State.Idle);
+            }
+        }
+
+        void UpdateMarkerPosition()
+        {
+            GazePoint gazePoint = TobiiAPI.GetGazePoint();
+            if (gazePoint.IsValid)
+            {
+                if (!hasStarted) InitTobii(gazePoint);
+                UpdatePosition(gazePoint);
+            }
+        }
+
+        void UpdatePosition(GazePoint gazePoint)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(gazePoint.Screen);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 1000f))
+            {
+                Vector3 markerPos = hit.point;
+                markerPos.y = marker.position.y;
+                marker.position = markerPos;
+            }
+        }
+    }
+}
